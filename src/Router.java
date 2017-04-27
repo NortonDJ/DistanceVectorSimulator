f23e6c67816cc058d188e9689630056d58f25cb2
nortondj <nortondj@lafayette.edu>
2017-04-27 16:18:50 -0400

    Instantiate router's neighbors' distance vectors to be null at initialization

--- src/Router.java
+++ src/Router.java
@@ -12,22 +12,31 @@ public class Router {
     private HashMap<SocketAddress, DistanceVector> vectorMap;
     private boolean poison;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
-    public Router(SocketAddress address){
+    public Router(SocketAddress address) {
         this.address = address;
         this.table = new ForwardingTable();
         this.neighborsMap = new HashMap<>();
         this.vectorMap = new HashMap<>();
+        addNeighborsToDistVectMap();
     }
 
     public Router(SocketAddress address, HashMap<SocketAddress, Integer> neighborsMap, boolean poison) {
         this.address = address;
         this.table = new ForwardingTable();
         this.neighborsMap = neighborsMap;
+        this.vectorMap = new HashMap<>();
         this.poison = poison;
+        addNeighborsToDistVectMap();
+    }
+
+    private void addNeighborsToDistVectMap() {
+        for (SocketAddress neighbor : neighborsMap.keySet()) {
+            this.vectorMap.put(neighbor, null);
+        }
     }
 
     /**
@@ -35,15 +44,16 @@ public class Router {
      * Calculates the new distance vector, and if the new vector is different
      * from the old one, the router updates the forwarding table and broadcasts
      * the new distance vector to its neighbors
+     *
      * @param vector the new distance vector
      */
-    public void receiveDistanceVector(DistanceVector vector){
+    public void receiveDistanceVector(DistanceVector vector) {
         this.vectorMap.put(vector.getSource(), vector);
 
         DistanceVectorCalculation oldCalculation = this.mostRecentCalculation;
         this.mostRecentCalculation = recalculateDistanceVector();
         // if a change has occurred
-        if(!mostRecentCalculation.equals(oldCalculation)){
+        if (!mostRecentCalculation.equals(oldCalculation)) {
             updateForwardingTable(mostRecentCalculation);
             broadCastDistanceVector(mostRecentCalculation);
         }
@@ -52,16 +62,17 @@ public class Router {
     /**
      * Recalculates the distance vector and paths to other nodes based on the
      * current distance vector map. The new distance vector is calculated as
-     * @<code>for all nodes the router knows about,
-     *     dsrc->node = min over all i{dsrc->neighbor(i) + dneighbor(i)->node}
-     * @</code>
+     *
      * @return the wrapper class for the DistanceVector and the Mapping of paths
      * for each destinations
+     * @<code>for all nodes the router knows about,
+     * dsrc->node = min over all i{dsrc->neighbor(i) + dneighbor(i)->node}
+     * @</code>
      */
-    public DistanceVectorCalculation recalculateDistanceVector(){
+    public DistanceVectorCalculation recalculateDistanceVector() {
         DistanceVector newVec = new DistanceVector(this.address);
         HashMap<SocketAddress, ArrayList<SocketAddress>> pathMap = new HashMap<>();
-        for(SocketAddress destination : vectorMap.keySet()){
+        for (SocketAddress destination : vectorMap.keySet()) {
 
             Integer minimum = Integer.MAX_VALUE;
             ArrayList<SocketAddress> path = new ArrayList<>();
@@ -70,7 +81,7 @@ public class Router {
             // so iterate over the neighbors
             // and choose the minimum
 
-            for(SocketAddress neighbor : neighborsMap.keySet()){
+            for (SocketAddress neighbor : neighborsMap.keySet()) {
 
                 //calculate distance = dsrc->neighbor + dneighbor->node
 
@@ -84,8 +95,8 @@ public class Router {
                 //         if same, then dneighbor->dest = dneighbor->neighbor = 0;
                 //         otherwise dneighbor->dest = inf, because router doesn't have path
 
-                if(neighborDistanceVector == null) {
-                    if(neighbor.equals(destination)){
+                if (neighborDistanceVector == null) {
+                    if (neighbor.equals(destination)) {
                         // destination and neighbor are the same
                         distanceNeighborToNode = 0;
                     } else {
@@ -98,10 +109,14 @@ public class Router {
                     distanceNeighborToNode = neighborDistanceVector.getValue(destination);
                 }
 
+                Integer calculation;
+                if (distanceToNeighbor == Integer.MAX_VALUE || distanceNeighborToNode == Integer.MAX_VALUE) {
+                    calculation = Integer.MAX_VALUE;
+                } else {
+                    calculation = distanceToNeighbor + distanceNeighborToNode;
+                }
 
-                Integer calculation = distanceToNeighbor + distanceNeighborToNode;
-
-                if(calculation < minimum){
+                if (calculation < minimum) {
                     path.clear();
                     path.add(this.address);
                     path.add(neighbor);
@@ -122,43 +137,44 @@ public class Router {
      * If the router is set to poison, then it will do so here by copying the
      * distance vector from the calculation and applying the poison on an individual
      * basis per neighbor.
+     *
      * @param calculation
      */
-    public void broadCastDistanceVector(DistanceVectorCalculation calculation){
+    public void broadCastDistanceVector(DistanceVectorCalculation calculation) {
         HashMap<SocketAddress, ArrayList<SocketAddress>> nextHops = calculation.getPathMap();
-        for(SocketAddress neighbor : neighborsMap.keySet()){
+        for (SocketAddress neighbor : neighborsMap.keySet()) {
             DistanceVector individualizedVec = calculation.getResultVector();
-            HashMap<SocketAddress,ArrayList<SocketAddress>> pathMap = calculation.getPathMap();
+            HashMap<SocketAddress, ArrayList<SocketAddress>> pathMap = calculation.getPathMap();
             ArrayList<SocketAddress> path = pathMap.getOrDefault(neighbor, new ArrayList<>());
 
-            if(poison){
+            if (poison) {
                 individualizedVec.applyPoison(neighbor, path);
             }
             sendDistanceVector(neighbor, individualizedVec);
         }
     }
 
-    public void updateForwardingTable(DistanceVectorCalculation calculation){
+    public void updateForwardingTable(DistanceVectorCalculation calculation) {
         System.out.println("UPDATE FORWARDING TABLE NOT SUPPORTED YET");
     }
 
-    public void sendDistanceVector(SocketAddress neighbor, DistanceVector distanceVector){
+    public void sendDistanceVector(SocketAddress neighbor, DistanceVector distanceVector) {
         System.out.println("SENDING NOT SUPPORTED YET");
     }
 
-    public String getIP(){
+    public String getIP() {
         return this.address.getIp();
     }
 
-    public Integer getPort(){
+    public Integer getPort() {
         return this.address.getPort();
     }
 
-    public boolean hasNeighbor(SocketAddress address){
+    public boolean hasNeighbor(SocketAddress address) {
         return neighborsMap.containsKey(address);
     }
 
-    public int getWeight(SocketAddress address){
+    public int getWeight(SocketAddress address) {
         return neighborsMap.getOrDefault(address, -1);
     }
 
