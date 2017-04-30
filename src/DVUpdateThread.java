0a876393687c1adcee9dd5fc445a6b07db16c477
nortondj <nortondj@lafayette.edu>
2017-04-30 04:25:00 -0400

    Add Thread class for updates in DV program

---
+++ src/DVUpdateThread.java
@@ -0,0 +1,16 @@
+/**
+ * Created by nortondj on 4/30/17.
+ */
+public class DVUpdateThread implements Runnable{
+
+    private Router r;
+
+    public DVUpdateThread(Router r){
+        this.r = r;
+    }
+
+    public void run(){
+        r.broadCastDistanceVector(r.getMostRecentCalculation());
+    }
+
+}
