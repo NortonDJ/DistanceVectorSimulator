0adcd221723490fe9f3c24aa416cc3b8828068fe
nortondj <nortondj@lafayette.edu>
2017-04-30 04:24:30 -0400

    Add Thread class for commands in DV program

---
+++ src/DVCommandThread.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/**
+ * Created by nortondj on 4/30/17.
+ */
+public class DVCommandThread implements Runnable {
+
+    private Router r;
+
+    public DVCommandThread(Router r) {
+        this.r = r;
+    }
+
+    public void run() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello, type !help for supported commands.\n");
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            if (lineScanner.hasNext()) {
+                String firstWord = lineScanner.next();
+                switch (firstWord) {
+                    case ("!help"): {
+                        System.out.println(
+                                "Supported commands: \n" +
+                                        "PRINT - print the current node's distance vector, and the distance vectors received from the neighbors.\n" +
+                                        "MSG <dst-ip> <dst-port> <msg> - send message msg to a destination with the specified address.\n" +
+                                        "CHANGE <dst-ip> <dst-port> <new-weight> - change the weight between the current node and the specified node to new-weight and update the specified node about the change.\n");
+                        break;
+                    }
+                    case ("PRINT"): {
+                        r.print();
+                        break;
+                    }
+                    case ("MSG"): {
+                        String destIp = lineScanner.next();
+                        String destPort = lineScanner.next();
+                        String message = "";
+                        while (lineScanner.hasNext()) {
+                            message += lineScanner.next();
+                        }
+                        SocketAddress address = new SocketAddress(destIp, Integer.parseInt(destPort));
+                        r.message(address);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
