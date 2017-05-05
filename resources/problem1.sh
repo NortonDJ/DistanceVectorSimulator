b71f8c1eb2dbfa20675bb50a9caf1fb82c209d77
nortondj <nortondj@lafayette.edu>
2017-05-05 09:38:00 -0400

    Make shell scripts relative to resources folder

--- resources/problem1.sh
+++ resources/problem1.sh
@@ -1,6 +1,6 @@
-xterm -title "A" -e "cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router ../../../resources/problem1a.txt" &
-xterm -title "B" -e "sleep 1; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router ../../../resources/problem1b.txt" &
-xterm -title "C" -e "sleep 2; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router ../../../resources/problem1c.txt" &
-xterm -title "D" -e "sleep 3; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router ../../../resources/problem1d.txt" &
-xterm -title "E" -e "sleep 4; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router ../../../resources/problem1e.txt"
+xterm -title "A" -e "cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem1a.txt" &
+xterm -title "B" -e "sleep 1; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem1b.txt" &
+xterm -title "C" -e "sleep 2; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem1c.txt" &
+xterm -title "D" -e "sleep 3; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem1d.txt" &
+xterm -title "E" -e "sleep 4; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem1e.txt"
 
--- resources/problem2poison.sh
+++ resources/problem2poison.sh
@@ -1,9 +1,9 @@
-xterm -title "A" -e "cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2a.txt" &
-xterm -title "B" -e "sleep 1; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2b.txt" &
-xterm -title "C" -e "sleep 2; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2c.txt" &
-xterm -title "D" -e "sleep 3; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2d.txt" &
-xterm -title "E" -e "sleep 4; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2e.txt" &
-xterm -title "F" -e "sleep 5; cd ~/IdeaProjects/DistanceVectorSimulator/out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2f.txt" 
+xterm -title "A" -e "cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2a.txt" &
+xterm -title "B" -e "sleep 1; cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2b.txt" &
+xterm -title "C" -e "sleep 2; cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2c.txt" &
+xterm -title "D" -e "sleep 3; cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2d.txt" &
+xterm -title "E" -e "sleep 4; cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2e.txt" &
+xterm -title "F" -e "sleep 5; cd ./../out/production/DistanceVectorSimulator; java Router -reverse ../../../resources/problem2f.txt; sleep 10;"
 
 
 
