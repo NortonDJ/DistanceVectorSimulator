487fd5106c08e295c92ea5f8376e92e60a474c0a
nortondj <nortondj@lafayette.edu>
2017-05-07 02:26:33 -0400

    Update problem2.sh to allow unlimited buffering in xterm

--- resources/problem2.sh
+++ resources/problem2.sh
@@ -2,7 +2,7 @@ xterm -title "A" -e "cd ./../out/production/DistanceVectorSimulator; java Router
 xterm -title "B" -e "sleep 1; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2b.txt" &
 xterm -title "C" -e "sleep 2; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2c.txt" &
 xterm -title "D" -e "sleep 3; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2d.txt" &
-xterm -title "E" -e "sleep 4; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2e.txt" &
+xterm -sl 1024 -title "E" -e "sleep 4; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2e.txt" &
 xterm -title "F" -e "sleep 5; cd ./../out/production/DistanceVectorSimulator; java Router ../../../resources/problem2f.txt"
 
 
