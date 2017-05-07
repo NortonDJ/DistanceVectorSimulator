5fa7954d477a7b55e94b1008338fcb72d1c7cb03
yizhong <chenyi@lafayette.edu>
2017-05-07 13:52:15 -0400

    DVcommand thread

--- src/DVCommandThread.java
+++ src/DVCommandThread.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Created by nortondj on 4/30/17.
+ * Implement Distance-Vector command
  */
 public class DVCommandThread implements Runnable {
 
     private Router r;
 
+    /**
+     * Constructor of DVCommandThread
+     */
     public DVCommandThread(Router r) {
         this.r = r;
     }
 
+    /**
+     * Implement Distance-Vector command:
+     *  1. PRINT - print the current node's distance vector, and the distance vectors received from the neighbors.
+     *  2. MSG <dst-ip> <dst-port> <msg> - send message msg to a destination with the specified address.
+     *  3. CHANGE <dst-ip> <dst-port> <new-weight> - change the weight between the current node 
+     *      and the specified node to new-weight and update the specified node about the change.
+     */
     public void run() {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello, type !help for supported commands.\n");
